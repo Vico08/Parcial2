66d7df88842c09686889bc7b4ca88798b0ffddc6
Victoria Cano <victoriacano@outlook.es>
2024-10-07 20:43:43 -0500

    bloque de while, do, if y switch

--- Parcial2/Program.cs
+++ Parcial2/Program.cs
@@ -6,7 +6,39 @@
         int numAleatorio = 0;
         int numJugadores = 0;
         bool playAgain  = true;
-        
+
+        while (playAgain)
+        {
+            Console.WriteLine("Bienvenidos al Juego Adivina el nùmero");
+            Console.WriteLine("Ingrese el numero de jugadores (2-4):");
+            
+            do
+            {
+                numJugadores = int.Parse(Console.ReadLine());
+                if (numJugadores < 2 || numJugadores > 4)
+                {
+                    Console.WriteLine("Número de jugadores inválido. Por favor, ingrese un número entre 2 y 4:");
+                }
+                while (numJugadores < 2 || numJugadores > 4);
+
+            switch (numJugadores)
+                {
+                    case 2:
+                        numeroAleatorio = random.Next(0, 51);
+                        break;
+                    case 3:
+                        numeroAleatorio = random.Next(0, 101);
+                        break;
+                    case 4:
+                        numeroAleatorio = random.Next(0, 201);
+                        break;
+                }
+
+
+
+
+
+            }
         
         
         
